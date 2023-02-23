c23cffdbb7caed801d4660a2398b45a60d17d629
Marcha <marchaf@gmail.com>
2023-02-23 09:25:18 +0100

    fix npm run build issue with mapbox

--- src/shared/components/UIElements/Map.jsx
+++ src/shared/components/UIElements/Map.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from "react";
-import mapboxgl from "mapbox-gl";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from "!mapbox-gl";
 
 import "./Map.css";
 
